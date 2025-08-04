c99825c23f0c31be48b0ffc1c4c570c72e61a386
kele_Bingtang <2456019588@qq.com>
2025-08-05 00:21:28 +0800

    feat(router): 完善路由权限控制并优化加载逻辑
    - 引入 FORBIDDEN_NAME 常量，用于处理无权限路由
    - 增加 menuVisibleWithForbidden 属性，控制菜单显示逻辑
    - 优化路由过滤逻辑，提高权限控制灵活性
    - 加载组件中引入路径缓存，避免重复加载
    - 在 dashboard 路由中添加角色权限控制

--- src/router/routes/dynamic/dashboard.ts
+++ src/router/routes/dynamic/dashboard.ts
@@ -14,6 +14,7 @@ const dashboardRoutes: RouterConfigRaw = {
       component: "/dashboard/console/index",
       meta: {
         title: "{{ _route.Console }}",
+        roles: ["1"],
       },
     },
     {
